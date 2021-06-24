a51c6c90a4e7096cca73556a98c1c05e8a7d66db
Apriadi <apriadi@bts.id>
2021-06-24 18:11:57 +0700

    styling header auth screen

--- src/screen/login/LoginScreen.js
+++ src/screen/login/LoginScreen.js
@@ -6,9 +6,9 @@ const LoginScreen = ({ navigation }) => {
     return(
         <View style={{flex : 1}}>
            <ImageBackground style={{flex : 1}} source={backgroundGambar}>
-            <View style={{flex: 1, justifyContent : 'flex-end', marginBottom : 60, marginHorizontal : 20}}>
-                  <View>
-                    <Button title={'Login'} color={'#009900'} onPress={() => navigation.navigate('PhoneLoginScreen')}/>
+            <View style={{flex: 1, justifyContent : 'flex-end', marginBottom : 110, marginHorizontal : 20}}>
+                  <View style={{ width: 230 }}>
+                    <Button title={'Login'} color={'#AAE296'} onPress={() => navigation.navigate('PhoneLoginScreen')}/>
                   </View>
               </View>
            </ImageBackground>
--- src/stack/LoginStack.js
+++ src/stack/LoginStack.js
@@ -9,7 +9,33 @@ const LoginStack = () => {
     return(
           <Stack.Navigator>
               <Stack.Screen name='LoginScreen' component={LoginScreen} options={{headerShown: false}}/>
-              <Stack.Screen name='PhoneLoginScreen' component={PhoneLoginScreen} options={{title : 'Log-In'}}/>
+              <Stack.Screen name='PhoneLoginScreen' component={PhoneLoginScreen} options={{
+                  title : 'Log-In',
+                  headerStyle: {
+                    backgroundColor: '#AAE296',
+                    shadowColor: "#000",
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+                    elevation: 5,
+                  },
+                  headerTintColor: '#fff',
+                  headerTitleStyle: {
+                    fontWeight: 'bold',
+                    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+                    textShadowOffset: {width: -1, height: 1},
+                    textShadowRadius: 10
+                  },
+                  headerBackTitleStyle:{
+                    fontWeight: 'bold',
+                    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+                    textShadowOffset: {width: -1, height: 1},
+                    textShadowRadius: 10
+                  }
+                  }}/>
           </Stack.Navigator>
       )
 }
