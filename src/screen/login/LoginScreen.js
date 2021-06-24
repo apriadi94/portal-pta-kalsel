33aee4eb5ac8154e6c48215056b43378187a424f
Apriadi <apriadi@bts.id>
2021-06-24 17:01:26 +0700

    menambahkan background image di awal

--- src/screen/login/LoginScreen.js
+++ src/screen/login/LoginScreen.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, ImageBackground, Button } from 'react-native'
+import backgroundGambar from '../../assets/background.png'
 
 const LoginScreen = ({ navigation }) => {
     return(
         <View style={{flex : 1}}>
-           <View style={{flex: 1, justifyContent : 'flex-end', marginBottom : 60, marginHorizontal : 20}}>
-                <View>
-                  <Button title={'Login'} color={'#84d198'} onPress={() => navigation.navigate('PhoneLoginScreen')}/>
-                </View>
-             </View>
+           <ImageBackground style={{flex : 1}} source={backgroundGambar}>
+            <View style={{flex: 1, justifyContent : 'flex-end', marginBottom : 60, marginHorizontal : 20}}>
+                  <View>
+                    <Button title={'Login'} color={'#009900'} onPress={() => navigation.navigate('PhoneLoginScreen')}/>
+                  </View>
+              </View>
+           </ImageBackground>
         </View>
     )
 }
