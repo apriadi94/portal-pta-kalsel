6311300640d0c0549831a4d31462cc11ae163282
Apriadi <dev.apriadi@gmail.com>
2021-06-25 05:59:51 +0700

    membuat list conversation

--- src/screen/chat/ChatScreen.js
+++ src/screen/chat/ChatScreen.js
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { ChatContext } from '../../provider/ChatProvider'
 
 const ChatScreen = ({ navigation }) => {
+    const { socket } = useContext(ChatContext)
+    const [conversation, setConversation] = useState([])
+    const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         navigation.setOptions({
             headerRight: () => (
@@ -12,11 +17,69 @@ const ChatScreen = ({ navigation }) => {
                 </TouchableOpacity>
               ),
         })
+
+        socket.emit('REQUEST_CONVERSATION')
+        socket.on('CONVERSATION_SENT', data => {
+          setConversation(data)
+          setLoading(false)
+        })
     }, [])
     return(
+      <View style={{flex: 1}}>
+      {loading ? (
         <View>
-            <Text>Chat Screen</Text>
+          <Text>Loading...</Text>
         </View>
+      ) : (
+        // eslint-disable-next-line react-native/no-inline-styles
+        <View style={{marginTop: 10, marginHorizontal : 10}}>
+          {conversation.map((list, index) =>
+              <TouchableOpacity
+                key={index}
+                onPress={() => {
+                  navigation.navigate('ChatContentScreen', { 
+                    to: [{ name: list.room.name }],
+                    room: { id: list.roomId, name: list.room.name, image: list.room.image }
+                  })
+                }}
+                
+                >
+                <View style={{backgroundColor: '#fff', marginBottom: 10, borderRadius : 10}}>
+                  <View
+                    style={{
+                      marginHorizontal: 10,
+                      marginVertical: 10,
+                      flexDirection: 'row',
+                    }}>
+                    <Image
+                      source={{uri: list.room.image}}
+                      style={{width: 60, height: 60, borderRadius: 30}}
+                    />
+                    <View>
+                        <Text style={{fontSize: 25, marginLeft: 20}}>
+                        {list.room.name}
+                        </Text>
+                        <View style={{flexDirection : 'row'}}>
+                          <Text numberOfLines={1} style={{marginLeft : 20, marginTop : 5, width : 270}}>
+                              {list.lastMessage}
+                          </Text>
+                          {
+                            list.unRead > 0 ?
+                            <View style={{width : 20, backgroundColor : 'pink', borderRadius : 10, justifyContent : 'center', alignItems : 'center'}}>
+                            <Text>
+                              {list.unRead}
+                            </Text>
+                          </View> : null
+                          }
+                        </View>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+          )}
+        </View>
+      )}
+    </View>
     )
 }
 
