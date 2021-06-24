6a756016e0d174e2c7a358bf04ca188ea92e4436
Apriadi <apriadi@bts.id>
2021-06-24 15:12:18 +0700

    menambahkan google authentikasi dan phone authentikasi

--- src/provider/AuthProvider.js
+++ src/provider/AuthProvider.js
@@ -1,10 +1,23 @@
-import React, { createContext } from 'react';
+import React, { useEffect, useState, createContext } from 'react';
+import auth from '@react-native-firebase/auth';
 
 export const AuthContext = createContext();
-
 export const AuthProvider = ({ children }) => {
 
-    const AuthState = {};
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
+
+    const onAuthStateChanged = async (user) => {
+        await setUser(user);
+        if (initializing) setInitializing(false);
+    }
+    
+      useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber;
+      }, []);
+
+    const AuthState = { user, initializing };
 
     return(
         <AuthContext.Provider value={AuthState}>
