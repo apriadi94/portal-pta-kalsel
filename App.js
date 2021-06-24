c4a938800ed35420d8eff4a484a928b035ecc6cd
Apriadi <apriadi@bts.id>
2021-06-24 15:04:16 +0700

    menambahkan provider

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { AuthProvider } from './src/provider/AuthProvider';
+import MainStack from './src/stack/MainStack';
 
 const App = () => {
   return (
-    
+    <AuthProvider>
+      <MainStack/>
+    </AuthProvider>
   );
 };
 
