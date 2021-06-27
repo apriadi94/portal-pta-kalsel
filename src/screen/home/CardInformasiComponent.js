d9419760e5a74ce0e88e309d96d0f0a2a1250524
Apriadi <dev.apriadi@gmail.com>
2021-06-27 11:12:23 +0700

    design tampilan beranda

---
+++ src/screen/home/CardInformasiComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CardInformasiComponent = () => {
+    return(
+        <View style={{marginTop : 5, backgroundColor: '#fff', height : 200, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Card Informasi</Text>
+        </View>
+    )
+}
+
+export default CardInformasiComponent
